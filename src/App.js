3c6ca98ec673e2a9c8720754bf1ef17a2af35b08
Daisyhan35 <daisyhan35@gmail.com>
2023-05-17 20:47:16 +0100

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -7,10 +8,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path='/' exact Component={Home} />
       </Routes>
+      <Footer />
       </Router>
       </div>
   );
