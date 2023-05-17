c20bbe868e069f19d66d5de8f052479ef00edf3b
Daisyhan35 <daisyhan35@gmail.com>
2023-05-17 19:23:14 +0100

    Website Nav

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BannerImage from '../assets/pizza.jpeg'
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='headerContainer' style={{ backgroundImage: `url(${BannerImage})` }}>
+        <h1>Daisyhan's Pizzeria</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to='/menu'>
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
