5a1c763e7be5715e1dc0a6cb758b45ac4f1c0e77
Daisyhan35 <daisyhan35@gmail.com>
2023-05-18 19:05:25 +0100

    Add Menu Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact Component={Home} />
+        <Route path='/menu' exact Component={Menu} />
+
       </Routes>
       <Footer />
       </Router>
