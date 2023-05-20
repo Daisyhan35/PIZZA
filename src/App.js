def4ff7abf5948e1f59810e5090cde327c6e6110
Daisyhan35 <daisyhan35@gmail.com>
2023-05-20 20:34:12 +0100

    Add About Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import About from './pages/About';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -14,6 +15,8 @@ function App() {
       <Routes>
         <Route path='/' exact Component={Home} />
         <Route path='/menu' exact Component={Menu} />
+        <Route path='/about' exact Component={About} />
+
 
       </Routes>
       <Footer />
